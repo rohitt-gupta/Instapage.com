496da88111541a5d808d26f3771d53b9af63eb02
rohitGupta2022 <pta.rohit28@gmail.com>
2023-03-03 10:13:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+const App = () => {
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</Router>
+	);
+};
+
+export default App;
